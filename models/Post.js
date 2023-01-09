27f11fbf439aff1ff976d209667aae2f1dae44d8
ivil2021 <89508514326@mail.ru>
2023-01-09 22:31:19 +0300

    change post model

--- models/Post.js
+++ models/Post.js
@@ -1,16 +1,25 @@
 ﻿import mongoose from 'mongoose';
 
 const PostSchema = new mongoose.Schema({
-    postText: {
+    title: {
         type: String,
         required: true, // свойство обязательно
         unique: true, // свойство должно быть уникальным
     },
+    text: {
+        type: String,
+        required: true, // свойство обязательно
+        unique: true, // свойство должно быть уникальным
+    },
+    viewsCount: {
+        type: Number,
+        default: 0,
+    },
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'User', // reference to model User // relationship between 2 tables
         required: true,
-    }
+    },
 },{
     timestamps: true
 });
